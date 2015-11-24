2b15cf72cc7d98af2e3b7bce022f0237acd2c399
RemyBrb <remy.barbe@etu.univ-tours.fr>
2015-11-24 20:47:15 +0100

    Push statistiques js

---
+++ src/main/webapp/resources/js/statistiques.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function()
+{
+   // On cache la zone de texte
+   jQuery('.TopRace').hide();
+   jQuery('.TopClazz').hide();
+   jQuery('.TopSpec').hide();
+   jQuery('.TopUsers').hide();
+   
+   jQuery('.TopRace').toggle("slow");
+   jQuery('.TopClazz').toggle("slow");
+   jQuery('.TopSpec').toggle("slow");
+   jQuery('.TopUsers').toggle("slow");
+   
+});
