67b67df247f95cc49835736d7411931d3d9d96f5
RemyBrb <remy.barbe@etu.univ-tours.fr>
2015-11-29 20:32:26 +0100

    ajout fade in block

--- src/main/webapp/resources/js/statistique.js
+++ src/main/webapp/resources/js/statistique.js
@@ -35,4 +35,19 @@ $(document).ready(function(){
 		$(elem).append(div);
 		
 	}
+	
+	   jQuery('.TopRace').hide();
+	   jQuery('.TopClazz').hide();
+	   jQuery('.TopSpec').hide();
+	   jQuery('.TopUsers').hide();
+	   jQuery('.UsersWithoutAvatar').hide();
+	   jQuery('.details').hide();
+	   
+		setTimeout(function() { 
+			$('.TopRace').fadeIn(); 
+			$('.TopClazz').delay(500).fadeIn();
+			$('.TopSpec').delay(1000).fadeIn();
+			$('.TopUsers').delay(1500).fadeIn();
+			$('.UsersWithoutAvatar').delay(2000).fadeIn();
+		}, 500);
 });
--- src/main/webapp/resources/js/statistiques.js
+++ src/main/webapp/resources/js/statistiques.js
@@ -1,20 +1,7 @@
 jQuery(document).ready(function()
 {
    // On cache la zone de texte
-   jQuery('.TopRace').hide();
-   jQuery('.TopClazz').hide();
-   jQuery('.TopSpec').hide();
-   jQuery('.TopUsers').hide();
-   jQuery('.UsersWithoutAvatar').hide();
-   jQuery('.details').hide();
-   
-	setTimeout(function() { 
-		$('.TopRace').fadeIn(); 
-		$('.TopClazz').delay(500).fadeIn();
-		$('.TopSpec').delay(1000).fadeIn();
-		$('.TopUsers').delay(1500).fadeIn();
-		$('.UsersWithoutAvatar').delay(2000).fadeIn();
-	}, 500);
+
 	
 	$('.element').click(function(event)
 			{
